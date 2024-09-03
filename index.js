6dc8e5d74d0aed6830f6ee2d11dedb9bd6ff700b
Dima <csourgg@gmail.com>
2024-09-03 09:53:12 +0300

    inition commit SERVER

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const PORT = 3000;
+const server = http.createServer((req, res)=>{
+    // console.log('req', req);
+    // console.log('res', res);
+    res.end( 'hello from server!' );
+});
+server.listen(PORT, ()=>{
+    console.log('server start at port '+PORT); 
+});
+//1025 - 
+
