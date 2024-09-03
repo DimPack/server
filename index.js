19ca9fc9fe73ccdc050023f606abfea79bf25ed1
Dima <csourgg@gmail.com>
2024-09-03 10:37:15 +0300

    add GET

--- index.js
+++ index.js
@@ -2,16 +2,39 @@ const http = require("http");
 const fs = require("fs");
 const PORT = 3000;
 const server = http.createServer((req, res) => {
-    console.log(req.method, req.url);
+    const {method, url} =req;
+    if (method === 'GET') {
+        if (url === '/') {
+            fs.readFile("./views/index.html", { encoding: "utf8" }, (err, data) => {
+                if (err) {
+                  console.log(err);
+                  return;
+                }
+                res.end(data);
+              });
+        }
+        if (url === '/about') {
+            fs.readFile("./views/adout.html", { encoding: "utf8" }, (err, data) => {
+                if (err) {
+                  console.log(err);
+                  return;
+                }
+                res.end(data);
+              });
+        }
+        if (url === '/contact') {
+            fs.readFile("./views/contact.html", { encoding: "utf8" }, (err, data) => {
+                if (err) {
+                  console.log(err);
+                  return;
+                }
+                res.end(data);
+              });
+        }
+    }
     
 
-  fs.readFile("./views/index.html", { encoding: "utf8" }, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.end(data);
-  });
+
   //read index.html
 //   res.end("hello from server!");
 });
